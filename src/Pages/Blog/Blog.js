ca0833008dd3e5b49875661b163755f15fffcbff
sumaiya7121 <sumaiya7121@gmail.com>
2022-11-09 22:00:53 +0600

    blog

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -3,7 +3,64 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            
+            <section className="dark:bg-gray-800 dark:text-gray-100">
+	<div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
+		<h2 className="text-2xl font-semibold sm:text-4xl">Frequently Asked Questions</h2>
+		<p className="mt-4 mb-8 dark:text-gray-400">Sagittis tempor donec id vestibulum viverra. Neque condimentum primis orci at lacus amet bibendum.</p>
+		<div className="space-y-4">
+			<details className="w-full border rounded-lg">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">What is the difference between SQL and NoSQL?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">A SQL Database follows a table like structure which can have an 
+unlimited number of rows and every data present inside the database 
+is properly structured with Predefined Schemas, it is basically used 
+to manipulate Relational Databases Management Systems.
+
+A NoSQL Database is a Distributed Database where the data is very 
+unstructured with Dynamic Schema. Unlike SQL it cannot have unlimited 
+rows but it follows a Standard Schema Definition and can store all 
+sorts of data models with large distributed data in the form of 
+key-value pairs, graph databases, documents or wide-column stores. </p>
+			</details>
+			<details className="w-full border rounded-lg">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">What is JWT, and how does it work?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">JSON Web Token (JWT) is an open standard (RFC 7519) for securely transmitting information between parties as JSON object.
+
+It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider(IdP). So the integrity and authenticity of the token can be verified by other parties involved.
+
+The purpose of using JWT is not to hide data but to ensure the authenticity of the data. JWT is signed and encoded, not encrypted. 
+
+JWT is a token based stateless authentication mechanism. Since it is a client-side based stateless session, server doesn't have to completely rely on a datastore(database) to save session information </p>
+			</details>
+			<details className="w-full border rounded-lg">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">What is the difference between javascript and NodeJS?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">1. NodeJS : 
+NodeJS is a cross-platform and opensource Javascript runtime environment that allows the javascript to be run on the server-side. Nodejs allows Javascript code to run outside the browser. Nodejs comes with a lot of modules and mostly used in web development. 
+
+2. JavaScript : 
+Javascript is a Scripting language. It is mostly abbreviated as JS. It can be said that Javascript is the updated version of the ECMA script. Javascript is a high-level programming language that uses the concept of Oops but it is based on prototype inheritance.  </p>
+			</details>
+
+            	<details className="w-full border rounded-lg">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">How does NodeJS handle multiple requests at the same time?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Given a NodeJS application, since Node is single threaded, say if processing involves a Promise.all that takes 8 seconds, does this mean that the client request that comes after this request would need to wait for eight seconds?
+No. NodeJS event loop is single threaded. The entire server architecture for NodeJS is not single threaded.
+
+Before getting into the Node server architecture, to take a look at typical multithreaded request response model, the web server would have multiple threads and when concurrent requests get to the webserver, the webserver picks threadOne from the threadPool and threadOne processes requestOne and responds to clientOne and when the second request comes in, the web server picks up the second thread from the threadPool and picks up requestTwo and processes it and responds to clientTwo. threadOne is responsible for all kinds of operations that requestOne demanded including doing any blocking IO operations.
+
+The fact that the thread needs to wait for blocking IO operations is what makes it inefficient. With this kind of a model, the webserver is only able to serve as much requests as there are threads in the thread pool.
+
+NodeJS Web Server maintains a limited Thread Pool to provide services to client requests. Multiple clients make multiple requests to the NodeJS server. NodeJS receives these requests and places them into the EventQueue .
+NodeJS server has an internal component referred to as the EventLoop which is an infinite loop that receives requests and processes them. This EventLoop is single threaded. In other words, EventLoop is the listener for the EventQueue.
+So, we have an event queue where the requests are being placed and we have an event loop listening to these requests in the event queue. What happens next?
+The listener(the event loop) processes the request and if it is able to process the request without needing any blocking IO operations, then the event loop would itself process the request and sends the response back to the client by itself.
+If the current request uses blocking IO operations, the event loop sees whether there are threads available in the thread pool, picks up one thread from the thread pool and assigns the particular request to the picked thread. That thread does the blocking IO operations and sends the response back to the event loop and once the response gets to the event loop, the event loop sends the response back to the client.
+
+How is NodeJS better than traditional multithreaded request response model?
+With traditional multithreaded request/response model, every client gets a different thread where as with NodeJS, the simpler request are all handled directly by the EventLoop. This is an optimization of thread pool resources and there is no overhead of creating the threads for every client request. </p>
+			</details>
+		</div>
+	</div>
+</section>
         </div>
     );
 };
